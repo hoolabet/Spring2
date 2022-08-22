c52bc3875b05a3909ee92b8e3043062f86ef66b6
GR <GR@DESKTOP-ODLO1F7>
2022-08-22 16:02:31 +0900

    memberVO

---
+++ Spring2/src/main/java/org/spring2/model/MemberVO.java
@@ -0,0 +1,90 @@
+package org.spring2.model;
+
+public class MemberVO {
+	private String id;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+	private String gender;
+	private String birth;
+	private String address1;
+	private String address2;
+	private String address3;
+	private String sign_date;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getBirth() {
+		return birth;
+	}
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	public String getAddress1() {
+		return address1;
+	}
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+	public String getAddress2() {
+		return address2;
+	}
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+	public String getAddress3() {
+		return address3;
+	}
+	public void setAddress3(String address3) {
+		this.address3 = address3;
+	}
+	public String getSign_date() {
+		return sign_date;
+	}
+	public void setSign_date(String sign_date) {
+		this.sign_date = sign_date;
+	}
+	@Override
+	public String toString() {
+		return "MemberVO [id=" + id + ", password=" + password + ", name=" + name + ", email=" + email + ", phone="
+				+ phone + ", gender=" + gender + ", birth=" + birth + ", address1=" + address1 + ", address2="
+				+ address2 + ", address3=" + address3 + ", sign_date=" + sign_date + "]";
+	}
+	
+	
+}
